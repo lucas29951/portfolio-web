1ddce5e2078e484e6d9ce9b4b83c59c72ee5ed21
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-05-01 02:01:54 -0300

    Se modificaron algunos estilos y se implemento la funcionalidad de Modo Oscuro en JavaScript

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,28 @@
 
+let allElements = document.querySelectorAll('*');
+
+let mainBody = document.querySelector('.main-body');
+
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+
+let themeIcon = document.querySelector('#change-theme');
+
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-bars');
     menuIcon.classList.toggle('fa-times');
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
+}
+
+
+themeIcon.onclick = () => {
+    themeIcon.classList.toggle('fa-sun');
+    themeIcon.classList.toggle('fa-moon');
+    mainBody.classList.toggle('dark-theme');
+    document.body.classList.toggle('dark-theme');
+    allElements.forEach(element => {
+        element.classList.toggle('dark-theme');
+    });
 }
